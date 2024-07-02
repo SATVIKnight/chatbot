362059a78f767dd23fc0429f339c9bfe96775240
Satvik Keshtwal <95976610+SATVIKnight@users.noreply.github.com>
2024-07-03 02:04:39 +0530

    Update product.dart
    my bad

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import 'package:excel/excel.dart';
 
 class Product {
@@ -35,41 +35,3 @@ class Product {
     );
   }
 }
-=======
-import 'package:excel/excel.dart';
-
-class Product {
-  final String companyName;
-  final String companyAddress;
-  final String contactPhone;
-  final String companyWebsite;
-  final String productName;
-  final String mainFunctions;
-  final String howToUse;
-  final String productSummary;
-
-  Product({
-    required this.companyName,
-    required this.companyAddress,
-    required this.contactPhone,
-    required this.companyWebsite,
-    required this.productName,
-    required this.mainFunctions,
-    required this.howToUse,
-    required this.productSummary,
-  });
-
-  factory Product.fromExcel(List<Data?> row) {
-    return Product(
-      companyName: row[0]?.value.toString() ?? '',
-      companyAddress: row[1]?.value.toString() ?? '',
-      contactPhone: row[2]?.value.toString() ?? '',
-      companyWebsite: row[3]?.value.toString() ?? '',
-      productName: row[4]?.value.toString() ?? '',
-      mainFunctions: row[5]?.value.toString() ?? '',
-      howToUse: row[6]?.value.toString() ?? '',
-      productSummary: row[7]?.value.toString() ?? '',
-    );
-  }
-}
->>>>>>> 9b09265a08b9dd805484ca3c2ad478f6648727d0
