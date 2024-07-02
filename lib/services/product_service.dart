6b72a6a72614d621050bd45e000009a174d31699
Satvik Keshtwal <95976610+SATVIKnight@users.noreply.github.com>
2024-07-03 02:06:07 +0530

    Update product_service.dart
    my bad again

--- lib/services/product_service.dart
+++ lib/services/product_service.dart
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:excel/excel.dart';
 import '../models/product.dart';
@@ -26,32 +26,4 @@ class ProductService {
         .toList();
   }
 }
-=======
-import 'package:flutter/services.dart' show rootBundle;
-import 'package:excel/excel.dart';
-import '../models/product.dart';
 
-class ProductService {
-  List<Product> _products = [];
-
-  Future<void> loadProducts() async {
-    final data = await rootBundle.load('assets/products.xlsx');
-    final bytes = data.buffer.asUint8List();
-    final excel = Excel.decodeBytes(bytes);
-
-    final sheet = excel.tables.keys.first;
-    final rows = excel.tables[sheet]?.rows ?? [];
-
-    _products = rows.skip(1).map((row) => Product.fromExcel(row)).toList();
-  }
-
-  List<Product> recommendProducts(String keyword) {
-    return _products
-        .where((product) =>
-            product.productName.contains(keyword) ||
-            product.mainFunctions.contains(keyword) ||
-            product.productSummary.contains(keyword))
-        .toList();
-  }
-}
->>>>>>> 9b09265a08b9dd805484ca3c2ad478f6648727d0
